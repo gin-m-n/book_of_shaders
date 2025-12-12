6c3412352aa545c1b231e0d4378b14f32596628e
gin-m-n <ginmnmnmn@gmail.com>
2025-12-12 21:23:54 +0900

    animate

--- shaders/shaping/sin.glsl
+++ shaders/shaping/sin.glsl
@@ -14,15 +14,10 @@ float plot(vec2 st, float plot) {
 }
 
 void main() {
-    float scale = 4.0;
+    float scale = 3.0;
     vec2 st = gl_FragCoord.xy / u_resolution * scale - scale / 2.0 ;
     
-    float y = sin(st.x * 2.0 * PI);
-    
     vec3 color = vec3(0.0);
-    
-    float pct = plot(st, y);
-    color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
     
     float xZero = smoothstep(-0.01, 0.0, st.x) - smoothstep(0.0, 0.01, st.x);
     color = color + xZero * vec3(1.0, 0, 0);
@@ -38,6 +33,15 @@ void main() {
     color = color + yMinus1 * vec3(1.0);
     float yPlus1 = smoothstep(0.99, 1.0, st.y) - smoothstep(1.0, 1.01, st.y);
     color = color + yPlus1 * vec3(1.0);
+    
+    float y = sin((st.x) * 2.0 * PI);
+    
+    float pct = 0.0;
+    if (st.x < u_time * 2.0 - scale / 2.0) {
+        pct = plot(st, y);
+    }
+    
+    color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
     
     gl_FragColor = vec4(color, 1.0);
 }
